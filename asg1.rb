fcbc9ba063d47ae6729dcba74e9e6b57b1fe3e6f
Khushal Patel <110890843+Khushal8320@users.noreply.github.com>
2023-02-20 11:16:55 +0530

    day4

--- asg1.rb
+++ asg1.rb
@@ -1,32 +1,58 @@
-class Lasagna
+module Identifier
+      def Identifier.Account(a1)
+      puts "identifier"
+      @@balance1=10000+a1
+      
+      puts "#{@@balance1}" 
+      
+      end
+      
+      def Identifier.ATM()
+           
+           pin=gets 
+          
+          realpin = 1213
+          if  pin.to_i == realpin
+            puts "same"
+            
+            puts "put to balance,withdrow,deposit"
+            
+            @userdecision=gets.chomp
+            @balance="balance"
+            @withdrow="withdrow"
+            
+            if "balance" == @userdecision
+                puts "your account balance is #{@@balance1}"
+                
+            elsif "withdrow" == @userdecision
+                 puts "widrow the money"
+                 withdrow=gets.chomp
+                 puts "#{withdrow}"
+                 newbalance = @@balance1 - withdrow.to_i
+                 @@balance1=newbalance
+                 puts "#{newbalance}"
+            
+            
+            elsif "deposit" == @userdecision
+                 puts "deposit"
+                 withdrow1=gets.chomp
+                 newbalance1 = @@balance1 + withdrow1.to_i
+                 @@balance1=newbalance1
+                 
+            end
+          end
+          
+          puts "#{@@balance1}"
+          
+      
+     
+      end
+      
 
-      EXPECTED_MINUTES_IN_OVEN=40
-      puts "#{EXPECTED_MINUTES_IN_OVEN}"
-      
-      
-      def emaining_minutes_in_oven(a1)
-           diff= EXPECTED_MINUTES_IN_OVEN - a1
-          puts "#{diff} "
-      end
-      
-      def remaining_minutes_in_oven(a1=30)
-          puts "EXPECTED_MINUTES_IN_OVEN-#{remaining_minutes_in_oven}"
-      end
-      def preparation_time_in_minutes(a2)
-           c1= 2 * a2
-           puts "#{c1}"
-      end
-      def total_time_in_minutes(a3,a4)
-           actual_minutes_in_ove=a4
-           b1= 2 * a3
-           total =actual_minutes_in_ove+b1
-           puts "actual time is #{total}"
-       end
- end
- Lasagna.new()
- lasagna = Lasagna.new
-lasagna.emaining_minutes_in_oven(30)
-lasagna.preparation_time_in_minutes(2)
-lasagna.total_time_in_minutes(3,20)
- 
 
+    end
+   
+Identifier.Account(1000)
+
+
+Identifier.ATM()
